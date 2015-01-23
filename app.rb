8bf923bdf676e8f1351c5eb2b126b5e5834047b2
Joey <Joedawn@Joey-2.local>
2015-01-23 12:08:10 -0800

    implement files into Sinatra for browser viewing

---
+++ app.rb
@@ -0,0 +1,35 @@
+require('sinatra')
+require('sinatra/reloader')
+also_reload("lib/**/*.rb")
+require("./lib/client")
+require("./lib/stylist")
+require("pg")
+
+DB = PG.connect({:dbname => "salon"})
+
+get("/") do 
+	@stylists = Stylist.all()
+	erb(:index)
+end
+
+get("/stylists/:id") do 
+	@stylist = Stylist.find(params.fetch("id").to_i())
+	erb(:stylist)
+end
+
+post("/clients") do 
+	client_name = params.fetch("client_name")
+	stylist_id = params.fetch("stylist_id").to_i()
+	client = Client.new({:client_name => client_name, :stylist_id => stylist_id})
+	client.save()
+	@stylist = Stylist.find(stylist_id)
+	erb(:stylist)
+end
+
+post("/stylists") do 
+	stylist_name = params.fetch("stylist_name")
+	stylist = Stylist.new({:stylist_name => stylist_name, :id = nil})
+	stylist.save()
+	@stylists = Stylist.all()
+	erb(:index)
+end
