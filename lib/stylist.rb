3c4c9d7e7665b171ccf501d254bf199b906dac7e
Joey <Joedawn@Joey-2.local>
2015-01-23 11:05:06 -0800

    add :save and :== methods with passing specs to stylist class

---
+++ lib/stylist.rb
@@ -0,0 +1,29 @@
+class Stylist
+	attr_reader(:stylist_name, :id)
+
+	define_method(:initialize) do |attributes|
+		@stylist_name = attributes.fetch(:stylist_name)
+		@id = attributes.fetch(:id)
+	end
+
+	define_singleton_method(:all) do
+		returned_stylists = DB.exec("SELECT * FROM stylists;")
+		stylists = []
+		returned_stylists.each() do |stylist|
+			stylist_name = stylist.fetch("stylist_name")
+			id = stylist.fetch("id").to_i()
+			stylists.push(Stylist.new({:stylist_name => stylist_name, :id => id}))
+		end
+		stylists
+	end
+
+	define_method(:save) do 
+		result = DB.exec("INSERT INTO stylists (stylist_name) VALUES ('#{@stylist_name}') RETURNING id;")
+		@id = result.first().fetch("id").to_i()
+	end
+
+	define_method(:==) do |another_stylist|
+		self.stylist_name().==(another_stylist.stylist_name()).&(self.id().==(another_stylist.id()))
+	end
+
+end
--- spec/stylist_spec.rb
+++ spec/stylist_spec.rb
@@ -1,11 +1,27 @@
-# require('spec_helper')
+require('spec_helper')
 
-# describe(Stylist) do
+describe(Stylist) do
 
-#   describe(".all") do 
-#     it("starts off with no stylists") do
-#     	expect(Stylist.all()).to(eq([]))
-#     end
-#   end
+  describe(".all") do 
+    it("is an empty list of stylists at first") do
+    	expect(Stylist.all()).to(eq([]))
+    end
+  end
 
-# end
+  describe("#save") do 
+  	it("lets you save stylists to the database") do 
+  		stylist = Stylist.new({:stylist_name => "GZA", :id => nil})
+  		stylist.save()
+  		expect(Stylist.all()).to(eq([stylist]))
+  	end
+  end
+
+  describe("#==") do 
+  	it("is the same stylist if they have the same stylist_name") do 
+  		stylist1 = Stylist.new({:stylist_name => "RZA", :id => nil})
+  		stylist2 = Stylist.new({:stylist_name => "RZA", :id => nil})
+  		expect(stylist1).to(eq(stylist2))
+  	end
+  end
+
+end
